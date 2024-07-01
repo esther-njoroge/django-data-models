d0a52ee6999fd0462369fd027e7321a9c10276a5
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-07-01 12:37:09 +0300

    commmit changes

--- teacher/models.py
+++ teacher/models.py
@@ -12,5 +12,6 @@ class Teacher(models.Model):
     office_hours = models.IntegerField()
     biography = models.TextField()
     course_taught = models.CharField(max_length = 20)
+    class_size = models.PositiveSmallIntegerField()
     def __str__(self):
         return f"{self.first_name}  {self.last_name}"
